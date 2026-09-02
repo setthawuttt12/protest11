a5a0f1a7ef59148d3492d1f33e03485d23aaa410
yootoshima <yootoshi53258@gmail.com>
2026-09-02 09:49:29 +0700

    update nav2

--- frontend/pages/Evaluatee/selfeva.vue
+++ frontend/pages/Evaluatee/selfeva.vue
@@ -3,7 +3,11 @@
         <v-row>
             <v-col cols="12">
                 <v-form @submit.prevent="saveScore">
-                    <h1 class="text-h5 font-weight-bold"></h1>
+                    <h1 class="text-h5 font-weight-bold">แบบประเมินตนเอง</h1>
+                    <v-card class="pa-2">
+                        <p >ผู้ใช้งาน : {{ user.fname }} {{ user.lname }}</p>
+                        <p >รอบการประเมิน : {{ user.round_sys }} ปี {{ user.year_sys }}</p>
+                    </v-card>
                 </v-form>
             </v-col>
         </v-row>
@@ -11,7 +15,13 @@
 </template>
 
 <script setup lang="ts">
+import axios from 'axios'
+import { eva } from '~/API/base';
 
+const user = ref<any>({})
+const saveScore = async()=>{
+
+}
 </script>
 
 <style scoped>

0cbc9f471593bc272b36f4df6a6dac5867d63553
bird01 <bird01@vecskill.ovec>
2026-09-02 13:34:41 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,20 @@ app.use('/api/auth',auth)
 
 //eva
 
-const self = require('./routes/Eva/selfeva')
-app.use('/api/Eva/selfeva',self)
+
+//commit
+const show_eva = require('./routes/Commit/show_eva')
+app.use('/api/Commit/show_eva',show_eva)
+
+const score_member2 = require('./routes/Commit/score_member')
+app.use('/api/Commit/score_member',score_member2)
+
+const save_score = require('./routes/Commit/save_score')
+app.use('/api/Commit/save_score',save_score)
+
+const check_confirm = require('./routes/Commit/check_confirm')
+app.use('/api/Commit/check_confirm',check_confirm)
+
 
 app.use((req,res)=> res.status(404).json({message:'route not found'}))
 app.listen(3001,()=>{
--- backend/routes/Eva/selfeva.js
+++ backend/routes/Eva/selfeva.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const db = require('../../db')
-const router = express.Router()
-const {verifyToken,requireRole} = require('../../middleware/authmiddleware')
-const path = require('path')
-const uploadDir = path.join(__dirname,'../../uploads/evadetil')
+// const express = require('express')
+// const db = require('../../db')
+// const router = express.Router()
+// const {verifyToken,requireRole} = require('../../middleware/authmiddleware')
+// const path = require('path')
+// const uploadDir = path.join(__dirname,'../../uploads/evadetil')
 
 
-router.get('/user',verifyToken,requireRole('ผู้รับการประเมินผล'),async(req,res)=>{
-    try {
-        const id_member = req.user.id_member
-        const [rows] = await db.query(`select * from tb_member m,tb_eva e,tb_system s where e.id_member and e.id_member=m.id_member and e.id_sys=s.id_sys order by e.id_eva desc`,[id_member])
-        res.json(rows[0])
-    } catch (error) {
-        console.error('erorr Get user',error)
-        res.status(500).json({message:'error get user'})
-    }
-})
+// router.get('/user',verifyToken,requireRole('ผู้รับการประเมินผล'),async(req,res)=>{
+//     try {
+//         const id_member = req.user.id_member
+//         const [rows] = await db.query(`select * from tb_member m,tb_eva e,tb_system s where e.id_member and e.id_member=m.id_member and e.id_sys=s.id_sys order by e.id_eva desc`,[id_member])
+//         res.json(rows[0])
+//     } catch (error) {
+//         console.error('erorr Get user',error)
+//         res.status(500).json({message:'error get user'})
+//     }
+// })
 
-router.get('/topic',verifyToken,requireRole('ผู้รับการประเมินผล')async(req,res)=>{
-    try {
-        const id_member = req.user.id_member
-        const [topics] = await db.query(`select * from tb_topic`)
-        const [indicates] = await db.query(`select * from tb_indicate`)
-        const result = topics.
-    } catch (error) {
+// router.get('/topic',verifyToken,requireRole('ผู้รับการประเมินผล')async(req,res)=>{
+//     try {
+//         const id_member = req.user.id_member
+//         const [topics] = await db.query(`select * from tb_topic`)
+//         const [indicates] = await db.query(`select * from tb_indicate`)
+//         const result = topics.
+//     } catch (error) {
         
-    }
-})
+//     }
+// })
 
-module.exports = router
+// module.exports = router
